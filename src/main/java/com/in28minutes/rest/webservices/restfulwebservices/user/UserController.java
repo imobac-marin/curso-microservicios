c889abe13d30fbaeb3810daa2d3753189e5f7d24
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-05-16 15:21:29 +0100

    vídeo nº029

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,11 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,40 +22,47 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserDaoService service;
-	
+
 	// GET /users
-	//retrieveAllUsers
+	// retrieveAllUsers
 	@GetMapping("/users")
-	public List<User> retrieveAllUsers(){
+	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
-	//GET /users/{id}
-	//retrieveUser(int id)
+
+	// GET /users/{id}
+	// retrieveUser(int id)
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public Resource<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
-		if (user == null){
-			throw new UserNotFoundException("id-"+ id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
 		}
-		return user;
+
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		
+		resource.add(linkTo.withRel("all-users"));
+
+		return resource;
 	}
-	
+
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
 		return ResponseEntity.created(location).build();
 	}
-	
+
 	@DeleteMapping("/users/{id}")
-	public void DeleteUser(@PathVariable int id){
+	public void DeleteUser(@PathVariable int id) {
 		User user = service.deleteById(id);
-		if (user == null){
-			throw new UserNotFoundException("id-"+ id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
 		}
 	}
 }
