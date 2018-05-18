e720485a1023d6f0fc468941f120c8bb67b2ea61
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-05-18 13:00:09 +0100

    vídeo nº044

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values (1, sysdate(), 'Pepe');
+insert into user values (2, sysdate(), 'Juan');
+insert into user values (3, sysdate(), 'Pedro');
+insert into user values (4, sysdate(), 'Jose');
