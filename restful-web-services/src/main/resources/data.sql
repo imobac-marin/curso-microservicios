19c2269bc24b79006d24b4763be4c6bae6e124b4
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-05-21 11:54:37 +0100

    reestructurando la rama

--- restful-web-services/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user values (100, sysdate(), 'Pepe');
-insert into user values (200, sysdate(), 'Juan');
-insert into user values (300, sysdate(), 'Pedro');
-insert into user values (400, sysdate(), 'Jose');
-insert into post values(11001, 'Mi primer Post', 100);
-insert into post values(11002, 'Mi segundo Post', 100);
